23bb0cff47b981cc1a87833981cdedb2e01cee98
cyblogerz <pranavajay2002@gmail.com>
2021-03-23 23:51:58 +0530

    Adding required files

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget> [
+          Expanded(
+            child:TextButton(
+              onPressed:() {
+                print('left is pressed');
+
+              },
+                child: Image.asset('images/dice1.png'),
+            ),
+          ),
+
+          Expanded(
+              child:TextButton(
+                onPressed: () {
+                  print('Right button is pressed');
+                },
+
+                  child: Image.asset('images/dice1.png'
+                  ),
+              ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
